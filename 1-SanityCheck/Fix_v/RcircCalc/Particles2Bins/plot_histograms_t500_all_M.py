40d417cd6bd411fde1a3340c2a55d76a212f718b
Juan <juanmcloaiza@gmail.com>
2016-04-03 00:29:03 +0200

    embellishing r histogram plot

--- 1-SanityCheck/Fix_v/RcircCalc/Particles2Bins/plot_histograms_t500_all_M.py
+++ 1-SanityCheck/Fix_v/RcircCalc/Particles2Bins/plot_histograms_t500_all_M.py
@@ -18,7 +18,18 @@ def get_rcirc(radius,nparts):
 	for r,n in zip(radius,nparts):
 		mean_r += r*n
 	return mean_r
-	
+
+#Set relevant dimensions:
+FigSize = 10
+FontSize = 20
+BorderWidth = 3
+pl.rcParams.update({'font.size': FontSize})
+pl.rcParams.update({'axes.linewidth': BorderWidth})
+#Open figure:
+pl.figure( figsize=( FigSize, FigSize ) )
+pl.tick_params(width=BorderWidth, length=FontSize, which='major')
+pl.tick_params(width=BorderWidth, length=0.3*FontSize, which='minor')
+
 
 snap=500
 print("#M_bh r_circ")
@@ -26,15 +37,15 @@ for i in [6,7,8,9]:
 	datafile = "./M"+str(i)+"_r_l_histogram_t"+str(snap)+".txt"
 	X = np.array(load_data(datafile))
 	label_this="$M_{\\rm BH} = 10^"+str(i)+" M_{\\odot}, t = "+str(snap)+"$"
-	imgplot = pl.plot(X[:,0], X[:,1],  label = label_this)
+	imgplot = pl.plot(X[:,0], X[:,1],'-', linewidth = 0.5*FontSize, label = label_this)
 	print("1e"+str(i-10)+" "+str(get_rcirc(X[:,0],X[:,1])))
 
 pl.legend(loc=2)
-pl.xlabel("$r$")
-pl.ylabel("$N_{\\rm part}$")
+pl.xlabel("$r$",fontsize=1.5*FontSize)
+pl.ylabel("$N_{\\rm part}$",fontsize=1.5*FontSize)
 #pl.xlim([0,0.03])
 #pl.ylim([0,0.30])
-pl.title("Circularization radius is where the peak is..." )
+#pl.title("Circularization radius is where the peak is..." )
 figfile = "Rcirc_t500_all_Mbh.png"
 pl.savefig(figfile)
 print("Figure saved to "+figfile)
