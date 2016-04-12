5d6257f5f6fc8030d0c7ebf8231de8209293dfa3
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2016-04-12 11:49:03 +0200

    added the modules needed to compile Gadget into a .sh file

---
+++ 1-SanityCheck/No_racc/P-GadgetJM-20140925/modules_needed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+module load fftw/2.1.5/gnu/4.9.2
+module load openmpi/1.8.3/gnu/4.9.2
+module load gsl/1.16/gnu/4.9.2
+module load gnu/4.9.2
+module load hwloc/1.10.0
+module load numactl/2.0.10  
