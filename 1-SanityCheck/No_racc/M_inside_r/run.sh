9ad7744cd380841cceb6d6f06e90c6c656e7e257
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2016-04-13 12:13:00 +0200

    Set by hand r_max = 0.03 for the histogram at t500

--- 1-SanityCheck/No_racc/M_inside_r/run.sh
+++ 1-SanityCheck/No_racc/M_inside_r/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for res in '100k' '250k' '500k' '750k' '001M';
 do
-	./particles2bins.x /scratch/jcarmona/No_racc_res_${res}/Output/snapshot_000 ./res_${res}_t000.txt
+	./particles2bins.x /scratch/jcarmona/No_racc_res_${res}/Output/snapshot_500 ./res_${res}_t500.txt
 done
