88bffc17f1f61e1b2249d6b8cba866edd3e7ebb7
Juan <juanmcloaiza@gmail.com>
2016-04-26 17:18:02 +0200

    Compare M(r) between simulations with r_acc = 1e-3 and no_racc

--- 1-SanityCheck/Racc_1e-3/M_inside_r/plot_M_inside_r_all_resolutions.py
+++ 1-SanityCheck/Racc_1e-3/M_inside_r/plot_M_inside_r_all_resolutions.py
@@ -19,6 +19,14 @@ def get_rcirc(radius,nparts):
 		mean_r += r*n
 	return mean_r
 
+def correct_mass(x,error):
+	if (x == 0):
+		x = error
+	else:
+		x = x * (1.0-error) + error
+	return x
+	
+
 #Set relevant dimensions:
 FigSize = 10
 FontSize = 20
@@ -32,17 +40,40 @@ pl.tick_params(width=BorderWidth, length=0.3*FontSize, which='minor')
 
 
 snap=5
-#print("#M_bh r_circ")
-for res in ['100k','250k','500k','750k','001M']:
+
+#Plot the simulations with accretion radius correcting for erros
+#in mass calculation:
+for res, error in zip(['100k','250k','500k','750k','001M'],[0.00147026, 0.000535889, 0.000154021, 7.73302e-05, 5.09935e-05]):
+	datafile = "./res_"+res+"_t"+str(snap)+"00.txt"
+	X = np.array(load_data(datafile))
+	M_cumulative = np.array(X[:,1])
+#	print('#'+res)
+	for i in range(1,len(M_cumulative)):
+		M_cumulative[i] += M_cumulative[i-1]
+	for i in range(1,len(M_cumulative)):
+		M_cumulative[i] = correct_mass(M_cumulative[i],error)
+#		print(X[i,0], M_cumulative[i])
+	label_this= res#+" particles, $t = "+str(snap)+"$"
+	imgplot = pl.loglog(X[:,0], M_cumulative,'--', linewidth = 0.2*FontSize, label = label_this)
+
+#Plot the same simulations without correction for mass calculation:
+for res, color in zip(['100k','250k','500k','750k','001M'],['b.','g.','r.','c.','m.']):
 	datafile = "./res_"+res+"_t"+str(snap)+"00.txt"
 	X = np.array(load_data(datafile))
 	M_cumulative = np.array(X[:,1])
 	for i in range(1,len(M_cumulative)):
 		M_cumulative[i] += M_cumulative[i-1]
-	label_this= res#+" particles, $t = "+str(snap)+"$"
-	imgplot = pl.loglog(X[:,0], M_cumulative,'-', linewidth = 0.5*FontSize, label = label_this)
-	#imgplot2 = pl.loglog(X[:,0], X[:,1],'--', linewidth = 0.5*FontSize, label = label_this)
-	#print("1e"+str(i-10)+" "+str(get_rcirc(X[:,0],X[:,1])))
+	imgplot = pl.loglog(X[:,0], M_cumulative,color, linewidth = 0.2*FontSize)
+
+#Plot the simulations without accretion radius:
+for res, color in zip(['100k','250k','500k','750k','001M'],['b-','g-','r-','c-','m-']):
+	datafile = "../../No_racc/M_inside_r/res_"+res+"_t"+str(snap)+"00.txt"
+	X = np.array(load_data(datafile))
+	M_cumulative = np.array(X[:,1])
+	for i in range(1,len(M_cumulative)):
+		M_cumulative[i] += M_cumulative[i-1]
+	imgplot = pl.loglog(X[:,0], M_cumulative,color, linewidth = 0.2*FontSize)
+
 
 pl.legend(loc=2)
 pl.xlabel("$r$",fontsize=1.5*FontSize)
