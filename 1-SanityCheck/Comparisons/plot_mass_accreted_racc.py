e399afcea07c438595239380cf7cc3acaa8ce0b6
Juan <juanmcloaiza@gmail.com>
2016-04-27 12:07:27 +0200

    mass accreted as function of r_acc and resolution. Not as expected

--- 1-SanityCheck/Comparisons/plot_mass_accreted_racc.py
+++ 1-SanityCheck/Comparisons/plot_mass_accreted_racc.py
@@ -25,34 +25,27 @@ pl.tick_params(width=BorderWidth, length=FontSize, which='major')
 pl.tick_params(width=BorderWidth, length=0.3*FontSize, which='minor')
 
 #plot data for Racc = 1e-3
-datafile = "../Racc_1e-3/M_inside_r/Npart.txt"
+datafile = "./macc_racc_res.txt"
 X = np.array(load_data(datafile))
-Resolution = X[:,0]
-M_accreted = X[:,4]
-r_acc = 1e-3
-label_this = '$r_{\\rm acc} = 10^{-3}$'
-imgplot = pl.loglog(Resolution, M_accreted,'bo', markersize = 0.7*FontSize, linewidth = 0.7*FontSize, label = label_this)
-
-#plot data for Racc = 5e-3
-datafile = "../Racc_5e-3/M_inside_r/Npart.txt"
-X = np.array(load_data(datafile))
-Resolution = X[:,0]
-M_accreted = X[:,4]
-r_acc = 1e-3
-label_this = '$r_{\\rm acc} = 5\\times10^{-3}$'
-imgplot = pl.loglog(Resolution, M_accreted,'ro', markersize = 0.7*FontSize, linewidth = 0.7*FontSize, label = label_this)
+R_acc = X[:,0]
+Res = ['dummy','100k','250k','500k','750k','1M']
+for i in range(1,6):
+	label_this = Res[i]
+	imgplot = pl.loglog(R_acc, X[:,i],'o', markersize = 0.7*FontSize, linewidth = 0.7*FontSize, label = label_this)
 
 #plot fit
-#x = np.linspace(1e5,1e6,1025)
-#y = 1.5e-3*(x/1e5)**-1.5
-#imgplot = pl.loglog(x,y,'k--',label = '$y \\sim x^{-3/2} $')
+x = np.linspace(0.8e-3,6e-3,1025)
+y = 2*x
+imgplot = pl.loglog(x,y,'k--',label = '$ N_{\\rm acc}/N_{\\rm part} \\sim r_{\\rm acc} $')
+for i in range(1,10):
+	imgplot = pl.loglog(x,x/i**2,'k--')
 
-pl.legend(loc=1)
-pl.xlabel("$N_{\\rm part}$",fontsize=1.5*FontSize)
+pl.legend(loc=2)
+pl.xlabel("$r_{\\rm acc}$",fontsize=1.5*FontSize)
 pl.ylabel("$N_{\\rm acc}/N_{\\rm part}$",fontsize=1.5*FontSize)
 #pl.xlim([2e-3,2e-2])
 #pl.ylim([1e-3,1e0])
 #pl.title("Circularization radius is where the peak is..." )
-figfile = "macc_res.png"
+figfile = "macc_racc.png"
 pl.savefig(figfile)
 print("Figure saved to "+figfile)
