7a6004dd06a292c384e606fb58d9e5cb97e803dc
Juan <juanmcloaiza@gmail.com>
2016-06-01 16:33:03 +0200

    Now the integral of the accretion rate gives the accreted mass

--- 1-SanityCheck/Accretion/compute_accretion_rate.py
+++ 1-SanityCheck/Accretion/compute_accretion_rate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as pl
 import numpy as np
+import sys
 
 def load_data(filename):
 	all_lines = []
@@ -21,53 +22,104 @@ def load_data(filename):
 			all_lines.append(line_data)
 	return all_lines
 
-for datafile in (
-[
-'Racc_1e-3_res_100k.txt',
-#'Racc_1e-3_res_250k.txt',
-#'Racc_1e-3_res_500k.txt',
-#'Racc_1e-3_res_750k.txt',
-'Racc_1e-3_res_001M.txt',
-'Racc_1e-4_res_100k.txt',
-#'Racc_1e-4_res_250k.txt',
-#'Racc_1e-4_res_500k.txt',
-#'Racc_1e-4_res_750k.txt',
-'Racc_1e-4_res_001M.txt',
-#'Racc_1e-4_res_004M.txt',
-'Racc_5e-3_res_100k.txt',
-#'Racc_5e-3_res_250k.txt',
-#'Racc_5e-3_res_500k.txt',
-#'Racc_5e-3_res_750k.txt',
-'Racc_5e-3_res_001M.txt',
-]):
+def mag(string):
+	if(string == 'M'):
+		return 1e6
+	elif(string == 'k'):
+		return 1e3
+	else:
+		print("Error, string "+string+" cannot be converted to number.")
+		sys.exit()
 
 
+def compute_acc_rate(Npart,Time):
+	A = np.zeros((len(Npart),int(len(Npart)/2)))
+	delta_t = np.zeros((len(Npart),int(len(Npart)/2)))
+	Amean = np.zeros(len(Npart))
+	dtmean = np.zeros(len(Npart))
+	Astd = np.zeros(len(Npart))
+
+#	count = 0
+#	for delta_i in range(3,int(len(A)/2)):
+#		for i in range(len(A)):
+#			if(i+delta_i >= len(A)):
+#				A[i,delta_i] = - ( Npart[-1] - Npart[i-delta_i] ) / ( Time[-1] - Time[i-delta_i] )
+#				delta_t[i,delta_i] = ( Time[-1] - Time[i-delta_i] )
+#			elif(i-delta_i < 0):
+#				A[i,delta_i] = - ( Npart[i+delta_i] - Npart[0] ) / (Time[i+delta_i] - Time[0])
+#				delta_t[i,delta_i] = (Time[i+delta_i] - Time[0])
+#			else:
+#				A[i,delta_i] = - ( Npart[i+delta_i] - Npart[i-delta_i] ) / (Time[i+delta_i] - Time[i-delta_i])
+#				delta_t[i,delta_i] = (Time[i+delta_i] - Time[i-delta_i])
+#
+	delta_i = 1
+	for i in range(len(A)):
+		if( ( i - delta_i >= 0 ) and ( i + delta_i < len(A) )):
+				Amean[i] = - ( Npart[i+delta_i] - Npart[i-delta_i] ) / (Time[i+delta_i] - Time[i-delta_i])
+				dtmean[i] = (Time[i+delta_i] - Time[i-delta_i]) / (2.0*delta_i)
+#		Amean[i] = np.mean(A[i,:])
+#		dtmean[i] = np.mean(delta_t[i,:])
+#		Astd[i] = np.std(A[i,:])
+
+	return Amean, dtmean
+
+def add_point(datafile,style,labelswitch=True):
+	#Read datafile
 	DATA = np.array(load_data(datafile))
 	Time = DATA[:,0] 
 	Npart = (DATA[:,1]) / float(DATA[0,1])
-	A = np.zeros((len(Npart),int(len(Npart)/2)))
-	Amean = np.zeros(len(Npart))
-	Astd = np.zeros(len(Npart))
 
-	count = 0
-	for delta_i in range(3,int(len(A)/2)):
-		for i in range(len(A)):
-			if(i+delta_i >= len(A)):
-				A[i,delta_i] = - ( Npart[-1] - Npart[i-delta_i] ) / ( Time[-1] - Time[i-delta_i] )
-			elif(i-delta_i < 0):
-				A[i,delta_i] = - ( Npart[i+delta_i] - Npart[-1] ) / (Time[i+delta_i] - Time[-1])
-			else:
-				A[i,delta_i] += - ( Npart[i+delta_i] - Npart[i-delta_i] ) / (Time[i+delta_i] - Time[i-delta_i])
+	#Obtain resolution and accretion radius
+	#from the filename:
+	r_acc = float(datafile[5:9])
+	resol = float(datafile[-8:-5]) * mag(datafile[-5])
 
-	for i in range(len(A)):
-		Amean[i] = np.mean(A[i,:])
-		Astd[i] = np.std(A[i,:])
+	#Calculate Accretion Rate
+	M_dot, dt = compute_acc_rate(Npart,Time)
 
-	#pl.plot(Time,Npart)
-	pl.semilogy(Time,Amean,label = datafile)
-	pl.legend()
-	#pl.plot(Time,Astd,'b-')
-	#pl.plot(Time,Amean-Astd,'r--')
-	#pl.plot(Time,Amean+Astd,'r--')
-	#pl.ylim([Npart[-1],Npart[0]])
+	#Check that the integrated accretion 
+	#rates are consistent with 
+	#the total accreted mass:
+	Tot_acc_mass = Npart[0] - Npart[-1]
+	Int_acc_mass = sum(M_dot * dt)
+	error = abs(Tot_acc_mass - Int_acc_mass)/Tot_acc_mass
+	print("resol = "+str(resol)+" r_acc = "+str(r_acc)+", accreted mass: "+str(Tot_acc_mass)+", "+str(Int_acc_mass)+", error:"+str(error) )
+
+	pl.semilogy( resol, Tot_acc_mass, style, label = "$r_{\\rm acc} ="+str(r_acc)+"$" )
+	if(labelswitch):
+		pl.legend()
+
+
+for datafile in (
+[
+'Racc_1e-3_res_100k.txt',
+'Racc_1e-3_res_250k.txt',
+'Racc_1e-3_res_500k.txt',
+'Racc_1e-3_res_750k.txt',
+'Racc_1e-3_res_001M.txt',
+]):
+	add_point(datafile,'ro',False)
+
+for datafile in (
+[
+'Racc_1e-4_res_100k.txt',
+'Racc_1e-4_res_250k.txt',
+'Racc_1e-4_res_500k.txt',
+'Racc_1e-4_res_750k.txt',
+'Racc_1e-4_res_001M.txt',
+'Racc_1e-4_res_004M.txt',
+]):
+	add_point(datafile,'bo',False)
+
+for datafile in (
+[
+'Racc_5e-3_res_100k.txt',
+'Racc_5e-3_res_250k.txt',
+'Racc_5e-3_res_500k.txt',
+'Racc_5e-3_res_750k.txt',
+'Racc_5e-3_res_001M.txt',
+]):
+	add_point(datafile,'go',False)
+
+
 pl.show()
