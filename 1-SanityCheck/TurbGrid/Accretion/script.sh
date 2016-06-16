3ba728b9eafb2c4dc4b9b3945834ed482cee0089
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2016-06-16 13:06:38 +0200

    Accretion not modified by initial turbulence

--- 1-SanityCheck/TurbGrid/Accretion/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for RACC in '1e-3' '5e-3' '1e-4'
-do
-	for RES in '250k' #'100k' '500k' '750k' '001M' '004M'
-	do
-		cp /scratch/jcarmona/Racc_${RACC}_res_${RES}/Output/Accretion.txt ./Accretion/Racc_${RACC}_res_${RES}.txt
-	done
-done
