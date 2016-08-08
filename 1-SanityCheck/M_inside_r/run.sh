ec39a7851741ac8174195c6c8d9e47a4dafc5bc0
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2016-08-08 15:41:24 +0200

    correcting filenames

--- 1-SanityCheck/M_inside_r/run.sh
+++ 1-SanityCheck/M_inside_r/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for res in '100k' '250k' '500k' '750k' '001M';
 do
-	./particles2bins.x /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_500 ./res_${res}_t000.txt
+	./particles2bins.x /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_500 ./res_${res}_t500.txt
 #	ls /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_* | tail -n 1
 done
 
