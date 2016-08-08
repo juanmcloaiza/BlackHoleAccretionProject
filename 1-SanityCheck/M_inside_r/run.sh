6a4155fd8cc04cdfd8bbb20027011288a7a1a260
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2016-08-08 15:34:57 +0200

    adding data for M_inside_r plot

--- 1-SanityCheck/M_inside_r/run.sh
+++ 1-SanityCheck/M_inside_r/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#for res in '100k' '250k' '500k' '750k' '001M' '002M' '004M' '008M' '010M';
-#do
-	#./particles2bins.x /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_000 ./res_${res}_t000.txt
-#	ls /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_* | tail -n 1
-#done
-
-for res in '001M' #'002M' '004M' '008M' '010M';
+for res in '100k' '250k' '500k' '750k' '001M';
 do
-	./particles2bins.x /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_500 ./res_${res}_t500.txt
+	./particles2bins.x /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_500 ./res_${res}_t000.txt
+#	ls /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_* | tail -n 1
 done
+
+#for res in '002M' '004M' '008M' '010M';
+#do
+#	./particles2bins.x /scratch/jcarmona/Racc_1e-4_res_${res}/Output/snapshot_050 ./res_${res}_t500.txt
+#done
